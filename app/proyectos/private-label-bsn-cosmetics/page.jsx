b4c6703f0dbce6912c4da396fa1ab2048d382ef5
Andrés Ortega <andres03ortega@gmail.com>
2024-08-14 17:06:46 +0200

    nuevos cambios vid

--- app/proyectos/private-label-bsn-cosmetics/page.jsx
+++ app/proyectos/private-label-bsn-cosmetics/page.jsx
@@ -18,12 +18,12 @@ export default function HomePage() {
             descripcion={ <>
                 Para el proyecto de comunicación de Empanadas Malvón, nos centramos en destacar la autenticidad y sabor único de sus empanadas a través de diversas plataformas de medios. Implementamos una estrategia integrada que incluyó redes sociales, publicidad en línea y colaboraciones con influencers locales para aumentar la visibilidad y atraer a una audiencia más amplia. Nuestro contenido visual, acompañado de narrativas atractivas, resaltaba la calidad y la variedad de sus productos, conectando emocionalmente con los consumidores y promoviendo la experiencia culinaria que ofrece Empanadas Malvón.
                </>}
-            videoproyecto="https://soyandres.es/ermo/video/ermo-proyectos-2024-header-desktop.mp4"
+            videoproyecto="https://www.ermo.es/videos/BSN_Wrap.mp4"
             explicacion={ <>
                 Para el proyecto de comunicación de Empanadas Malvón, nos centramos en destacar la autenticidad y sabor único de sus empanadas a través de diversas plataformas de medios. Implementamos una estrategia integrada que incluyó redes sociales, publicidad en línea y colaboraciones con influencers locales para aumentar la visibilidad y atraer a una audiencia más amplia. Nuestro contenido visual, acompañado de narrativas atractivas, resaltaba la calidad y la variedad de sus productos, conectando emocionalmente con los consumidores y promoviendo la experiencia culinaria que ofrece Empanadas Malvón.
                </>}
-            image1="https://soyandres.es/ermo/video/ermo-proyectos-2024-header-desktop.mp4"
-            image2="https://soyandres.es/ermo/video/ermo-proyectos-2024-header-desktop.mp4"
+            image1="https://www.ermo.es/videos/BSN_mini_1.mp4"
+            image2="https://www.ermo.es/videos/BSN_mini_2.mp4"
         ></Proyectos>
       
 
