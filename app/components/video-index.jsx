3bc86d258c65ea1247a4317d41892868b8ab1120
Andrés Ortega <andres03ortega@gmail.com>
2024-08-22 16:31:54 +0200

    arreglado el problema de layout

--- app/components/video-index.jsx
+++ app/components/video-index.jsx
@@ -1,27 +1,31 @@
 "use client";
 
-const VideoIndex = ({ desktop, mobile }) => (
-  <div className="header__video">
-    <video
-      className="header__video-desktop"
-      autoPlay
-      loop
-      muted
-      playsInline
-      type="video/mp4"
-      src={desktop}
-    />
-    <video
-      className="header__video-mobile"
-      autoPlay
-      loop
-      muted
-      playsInline
-      type="video/mp4"
-      src={mobile}
-    />
-  </div>
-);
+import React from 'react';
+
+function VideoIndex({ desktop, mobile }) {
+  return (
+    <div className="header__video">
+      <video
+        className="header__video-desktop"
+        autoPlay
+        loop
+        muted
+        playsInline
+        type="video/mp4"
+        src={desktop}
+      />
+      <video
+        className="header__video-mobile"
+        autoPlay
+        loop
+        muted
+        playsInline
+        type="video/mp4"
+        src={mobile}
+      />
+    </div>
+  );
+}
 
 VideoIndex.displayName = "VideoIndex";
 
