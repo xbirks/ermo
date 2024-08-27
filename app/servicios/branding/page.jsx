65451d945a310e8b0fb12cb8dd6d0ef950d65877
Andrés Ortega <andres03ortega@gmail.com>
2024-08-27 22:03:00 +0200

    actualizado branding

--- app/servicios/branding/page.jsx
+++ app/servicios/branding/page.jsx
@@ -1,23 +1,20 @@
 import Servicios from '@/app/components/servicios.jsx';
 import ProySimilar from '@/app/components/proyecto-similar.jsx';
 import FotografiaLayout from './layout.jsx';
-import Image from 'next/image.js';
 import ServiciosImg from '@/app/components/servicios-img.jsx';
 
 
 //IMAGENES 
-import foto1 from '@/app/assets/img/servicios/MANOLO-BERNABEU-1.jpg';
-import foto2 from '@/app/assets/img/servicios/BSN(13).jpg';
-import foto3 from '@/app/assets/img/servicios/Delibreads-1.jpg';
-import foto4 from '@/app/assets/img/servicios/WAY-6.jpg';
-import foto5 from '@/app/assets/img/servicios/Delgo-53.jpg';
-import foto6 from '@/app/assets/img/servicios/_DSF3001.jpg';
-import foto7 from '@/app/assets/img/servicios/sin-título-1-Editar.png';
-import foto8 from '@/app/assets/img/servicios/Baswtones_juntos.jpg';
-import foto9 from '@/app/assets/img/servicios/Agosto_Wao_post-3.jpg';
-import foto10 from '@/app/assets/img/servicios/vino.jpg';
-import foto11 from '@/app/assets/img/servicios/principal.jpg';
-import foto12 from '@/app/assets/img/servicios/11(1).jpg';
+import foto1 from '@/app/assets/img/servicios/branding/bra1.jpg';
+import foto2 from '@/app/assets/img/servicios/branding/bra2.jpg';
+import foto3 from '@/app/assets/img/servicios/branding/bra3.jpg';
+import foto4 from '@/app/assets/img/servicios/branding/bra4.jpg';
+import foto5 from '@/app/assets/img/servicios/branding/bra5.jpg';
+import foto6 from '@/app/assets/img/servicios/branding/bra6.jpg';
+import foto7 from '@/app/assets/img/servicios/branding/bra7.jpg';
+import foto8 from '@/app/assets/img/servicios/branding/bra8.jpg';
+import foto9 from '@/app/assets/img/servicios/branding/bra9.jpg';
+import foto10 from '@/app/assets/img/servicios/branding/bra10.jpg';
 
 
 
@@ -26,8 +23,8 @@ export default function FotografiaPage() {
 
     <FotografiaLayout>
         <Servicios
-            desktop="https://www.ermo.es/videos/fotografia/FOTO_Desktop.mp4"
-            mobile="https://www.ermo.es/videos/fotografia/FOTO_Mobile.mp4"
+            desktop="https://www.ermo.es/videos/branding/BRANDING_Desktop.mp4"
+            mobile="https://www.ermo.es/videos/branding/BRANDING_Mobile.mp4"
 
             servicio="Identidad corporativa y branding"
            
@@ -52,8 +49,6 @@ export default function FotografiaPage() {
             <ServiciosImg src={foto8} alt="img1" link="#"></ServiciosImg>
             <ServiciosImg src={foto9} alt="img1" link="#"></ServiciosImg>
             <ServiciosImg src={foto10} alt="img1" link="#"></ServiciosImg>
-            <ServiciosImg src={foto11} alt="img1" link="#"></ServiciosImg>
-            <ServiciosImg src={foto12} alt="img1" link="#"></ServiciosImg>
         </div>
 
         <ProySimilar></ProySimilar>
