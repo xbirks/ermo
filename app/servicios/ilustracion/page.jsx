ad6a0b0808955effe6af4044d1b299f7bc0129e7
Andrés Ortega <andres03ortega@gmail.com>
2024-08-27 17:49:31 +0200

    añadidos material gráfico

--- app/servicios/ilustracion/page.jsx
+++ app/servicios/ilustracion/page.jsx
@@ -3,22 +3,17 @@ import ProySimilar from '@/app/components/proyecto-similar.jsx';
 import FotografiaLayout from './layout.jsx';
 import Image from 'next/image.js';
 import ServiciosImg from '@/app/components/servicios-img.jsx';
+import ServiciosVid from '@/app/components/servicios-vid.jsx';
 
 
 //IMAGENES 
-import foto1 from '@/app/assets/img/servicios/MANOLO-BERNABEU-1.jpg';
-import foto2 from '@/app/assets/img/servicios/BSN(13).jpg';
-import foto3 from '@/app/assets/img/servicios/Delibreads-1.jpg';
-import foto4 from '@/app/assets/img/servicios/WAY-6.jpg';
-import foto5 from '@/app/assets/img/servicios/Delgo-53.jpg';
-import foto6 from '@/app/assets/img/servicios/_DSF3001.jpg';
-import foto7 from '@/app/assets/img/servicios/sin-título-1-Editar.png';
-import foto8 from '@/app/assets/img/servicios/Baswtones_juntos.jpg';
-import foto9 from '@/app/assets/img/servicios/Agosto_Wao_post-3.jpg';
-import foto10 from '@/app/assets/img/servicios/vino.jpg';
-import foto11 from '@/app/assets/img/servicios/principal.jpg';
-import foto12 from '@/app/assets/img/servicios/11(1).jpg';
-
+import dibujo1 from '@/app/assets/img/servicios/ilustracion/dibujo1.mp4';
+import dibujo2 from '@/app/assets/img/servicios/ilustracion/dibujo2.jpg';
+import dibujo3 from '@/app/assets/img/servicios/ilustracion/dibujo3.jpg';
+import dibujo4 from '@/app/assets/img/servicios/ilustracion/dibujo4.mp4';
+import dibujo5 from '@/app/assets/img/servicios/ilustracion/dibujo5.mp4';
+import dibujo6 from '@/app/assets/img/servicios/ilustracion/dibujo6.jpg';
+import dibujo7 from '@/app/assets/img/servicios/ilustracion/dibujo7.jpg';
 
 
 export default function FotografiaPage() {
@@ -26,8 +21,8 @@ export default function FotografiaPage() {
 
     <FotografiaLayout>
         <Servicios
-            desktop="https://www.ermo.es/videos/fotografia/FOTO_Desktop.mp4"
-            mobile="https://www.ermo.es/videos/fotografia/FOTO_Mobile.mp4"
+            desktop="https://www.ermo.es/videos/ilustracion/ILUSTRACION_Desktop.mp4"
+            mobile="https://www.ermo.es/videos/ilustracion/ILUSTRACION_Mobile.mp4"
 
             servicio="Ilustración"
            
@@ -42,18 +37,13 @@ export default function FotografiaPage() {
         ></Servicios>
 
         <div className="servicios__imagenes master__body">
-            <ServiciosImg src={foto1} alt="img1" link="#"></ServiciosImg>
-            <ServiciosImg src={foto2} alt="img1" link="#"></ServiciosImg>
-            <ServiciosImg src={foto3} alt="img1" link="#"></ServiciosImg>
-            <ServiciosImg src={foto4} alt="img1" link="#"></ServiciosImg>
-            <ServiciosImg src={foto5} alt="img1" link="#"></ServiciosImg>
-            <ServiciosImg src={foto6} alt="img1" link="#"></ServiciosImg>
-            <ServiciosImg src={foto7} alt="img1" link="#"></ServiciosImg>
-            <ServiciosImg src={foto8} alt="img1" link="#"></ServiciosImg>
-            <ServiciosImg src={foto9} alt="img1" link="#"></ServiciosImg>
-            <ServiciosImg src={foto10} alt="img1" link="#"></ServiciosImg>
-            <ServiciosImg src={foto11} alt="img1" link="#"></ServiciosImg>
-            <ServiciosImg src={foto12} alt="img1" link="#"></ServiciosImg>
+            <ServiciosVid src={dibujo1} link="#"></ServiciosVid>
+            <ServiciosImg src={dibujo2} alt="img1" link="#"></ServiciosImg>
+            <ServiciosImg src={dibujo3} alt="img1" link="#"></ServiciosImg>
+            <ServiciosVid src={dibujo4} link="#"></ServiciosVid>
+            <ServiciosVid src={dibujo5} link="#"></ServiciosVid>
+            <ServiciosImg src={dibujo6} alt="img1" link="#"></ServiciosImg>
+            <ServiciosImg src={dibujo7} alt="img1" link="#"></ServiciosImg>
         </div>
 
         <ProySimilar></ProySimilar>
