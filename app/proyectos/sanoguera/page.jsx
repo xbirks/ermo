36a06a74ac681b65f05008c33a5bf3e38ce93fd1
Andrés Ortega <andres03ortega@gmail.com>
2024-09-02 22:44:17 +0200

    corregido error responsive vid

--- app/proyectos/sanoguera/page.jsx
+++ app/proyectos/sanoguera/page.jsx
@@ -8,15 +8,15 @@ export default function SanogueraPage() {
 
     <SanogueraLayout>
         <Proyectos
-            desktop="https://www.ermo.es/videos/sanoguera/SANOGUERA_Desktop.mp4"
-            mobile="https://www.ermo.es/videos/sanoguera/SANOGUERA_Mobile.mp4"
+            desktop="https://www.ermo.es/videos/sanoguera/SANOGUERA_Desktop.webm"
+            mobile="https://www.ermo.es/videos/sanoguera/SANOGUERA_Mobile.webm"
             cliente="Sanoguera Consultoría"
             tipo="Branding | Diseño web | Programación web | Fotografía corporativa"
             ano="2023 | 2024"
             descripcion={ <>
                 Renovación 360 de toda la imagen de marca de la empresa motivada por un a transición de Asesoría hacia Consultoría. Hicimos desde el rebranding de la antigua marca hasta el rediseño de una nueva web más atractiva y modernizada.
                </>}
-            videoproyecto="https://www.ermo.es/videos/sanoguera/SANOGUERA_Wrap.mp4"
+            videoproyecto="https://www.ermo.es/videos/sanoguera/SANOGUERA_Wrap.webm"
             explicacion={ <>
                 Las imágenes que hemos creado destacan la forma y textura de los envases, que, a pesar de ser genéricos, están bien pensados para ser reetiquetados por los clientes con sus propios diseños. Hemos hecho hincapié en la claridad y precisión para que los clientes puedan visualizar cómo se integrarán estos envases en sus propias líneas de productos.
                 <br></br><br></br>
@@ -25,8 +25,8 @@ export default function SanogueraPage() {
                 Nuestras fotografías ayudan a simplificar el proceso de compra de los clientes, mostrando de forma efectiva cómo los productos pueden ser un añadido valioso para sus ofertas de cuidado de la piel y cosméticos. Y mostrando como podrían verse representados en fotografía los productos reetiquetados finales.
             </>}
             
-            image1="https://www.ermo.es/videos/sanoguera/SANOGUERA_mini_2.mp4"
-            image2="https://www.ermo.es/videos/sanoguera/SANOGUERA_mini_3.mp4"
+            image1="https://www.ermo.es/videos/sanoguera/SANOGUERA_mini_2.webm"
+            image2="https://www.ermo.es/videos/sanoguera/SANOGUERA_mini_3.webm"
         ></Proyectos>
     </SanogueraLayout>
       
