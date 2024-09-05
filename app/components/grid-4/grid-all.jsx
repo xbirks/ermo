3e4f824368c2168fe54bd37ebcab65a3a533d472
Andrés Ortega <andres03ortega@gmail.com>
2024-09-05 19:50:00 +0200

    cambiado video web

--- app/components/grid-4/grid-all.jsx
+++ app/components/grid-4/grid-all.jsx
@@ -49,15 +49,23 @@ function GridAll() {
             />
 
 <Grid4Item
-              title="Consum Supermercados"
+              title="Delibreads"
               description="Animaciones en 2D para supermercados Consum, diseñadas para resaltar sus valores de sostenibilidad y comunidad en campañas de redes sociales y televisión. Este proyecto fue realizado en colaboración con la agencia de publicidad WAY."
               thumbnailSrc="https://ermo.es/videos/03_project_ERMO.webm"
               link="/not-found"
               altAtribute="Animación en 2D para Consum, destacando la frescura y sostenibilidad de sus productos en una campaña de redes sociales."
             />
                         
-          <Grid4Item
-              title="Ice Balls - Cubers"
+            <Grid4Item
+              title="Delgo"
+              description="Fotografía de producto y lifestyle para los hielos de Cubers. Destacando la belleza sin adulterar de su hielo y el diseño único de sus Ice Balls. También creamos contenido animado para sus plataformas de redes sociales, con las que aumentamos su presencia digital."
+              thumbnailSrc="https://ermo.es/videos/04_project_ERMO.webm"
+              link="/not-found"
+              altAtribute="Imagen vibrante de los cubitos de hielo premium de Cubers, mostrando su perfecta claridad y forma única."
+            />
+
+            <Grid4Item
+              title="Helados Estiu"
               description="Fotografía de producto y lifestyle para los hielos de Cubers. Destacando la belleza sin adulterar de su hielo y el diseño único de sus Ice Balls. También creamos contenido animado para sus plataformas de redes sociales, con las que aumentamos su presencia digital."
               thumbnailSrc="https://ermo.es/videos/04_project_ERMO.webm"
               link="/not-found"
