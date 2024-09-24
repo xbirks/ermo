45b68c27974997995b985e052fb31f9434b7ca4f
Andrés Ortega <andres03ortega@gmail.com>
2024-09-24 20:39:01 +0200

    error ortografico

--- app/components/Index/IndexIntro.jsx
+++ app/components/Index/IndexIntro.jsx
@@ -21,11 +21,11 @@ function IndexIntro({municipio}){
         IndexDescription={ <>
            <strong>No somos solo diseñadores;</strong> nos vemos como narradores visuales. Creamos contenido gráfico que deja huella.<br></br><br></br>
 
-          nos tomamos en serio cada detalle para asegurarnos de que lo que hacemos sea auténtico y diferente. <br></br><br></br>
+          Nos tomamos en serio cada detalle para asegurarnos de que lo que hacemos sea auténtico y diferente. <br></br><br></br>
 
             Sabemos que el mundo cambia rápido, y por eso <strong>ayudamos a las empresas</strong> a destacarse y adaptarse con soluciones que mezclan <strong>innovación y creatividad</strong>. Queremos darle vida a tu objetivo empresarial con un enfoque joven y dinámico, pero siempre manteniendo la búsqueda de la eficacia. <br></br><br></br>
 
-            Hemos trabajado con marcas y proyectos que inspiran, han dado resultados y son auténticas y diferentes. Conoce que proyectos hemos hecho <Link href="/proyectos/principal">aquí.</Link><br></br><br></br>
+            Hemos trabajado con marcas que inspiran, han dado resultados y son auténticas y diferentes. Conoce que proyectos hemos hecho <Link href="/proyectos/principal">aquí.</Link><br></br><br></br>
 
             Estamos en {municipio}, ¿te animas a dar forma a tu nueva idea con nosotros? 
           </>}
